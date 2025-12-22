6039889557dde0d4a42db83db72fc8dff4fa5baa
yzyzxjxx <129159856+yzyzxjxx@users.noreply.github.com>
2025-12-22 14:22:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLI
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'kaixin.yzyzxjxx.qzz.io';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYzJiOTU4ZWM3Y2FjODU4ODFlNTUxMGFkOGVhNDVmYjMiLCJ0IjoiMTQzNDU1NzUtNmJjMS00Yjc4LWIxMGUtY2QwMGZmNGMwNmI2IiwicyI6IlptSTVOemhoWWpndE1tVTVZeTAwT0RRMkxUbGtabUl0T0dFMU5UTXpZek0yTnprMiJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'saas.sin.fan';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || 'yzyz'                   // 
+const NAME = process.env.NAME || 'kaixin'                   // 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
   fs.mkdirSync(FILE_PATH);
